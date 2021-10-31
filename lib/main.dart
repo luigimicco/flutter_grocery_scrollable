cc770dd33c373bc54208b768e10b25d72038d134
luigimicco <luigi.micco@gmail.com>
2021-10-31 12:51:16 +0100

    scaffold for grocery items

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class Fooderlich extends StatelessWidget {
         providers: [
           // 2
           ChangeNotifierProvider(create: (context) => TabManager()),
-          // TODO 10: Add GroceryManager Provider
+          ChangeNotifierProvider(create: (context) => GroceryManager()),
         ],
         child: const Home(),
       ),
